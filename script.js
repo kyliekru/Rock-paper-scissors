0f49d0b828c300732f04e2f9cf32be9762d51b7c
Kylie K <kylieakrueger@gmail.com>
2022-04-13 10:08:26 -0700

    Add playRound function

--- script.js
+++ script.js
@@ -2,18 +2,53 @@ function computerPlay() {
     let ranNum = Math.floor(Math.random() * 3);
     let choice;
     if (ranNum === 0) {
-        choice = "Rock";
+        choice = "rock";
         return choice;
     } else if (ranNum === 1) {
-        choice = "Paper";
+        choice = "paper";
         return choice;
     } else if (ranNum === 2) {
-        choice = "Scissors";
+        choice = "scissors";
         return choice;
     } else {
         console.log('error!');
     }
     console.log(choice);
     return choice;
+}
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let winCounter = 0;
+    let lossCounter = 0;
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        console.log('It\'s a tie!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        lossCounter += 1;
+        console.log('Paper beats rock! Computer wins.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        winCounter += 1;
+        console.log('Rock beats scissors! You win.');
+    }
+
+    if (playerSelection === 'paper' && computerSelection === 'paper') {
+        console.log('It\'s a tie!');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        winCounter += 1;
+        console.log('Paper beats rock! You win.');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        lossCounter += 1;
+        console.log('Scissors beats paper! Computer wins.');
+    }
+
+    if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        console.log('It\'s a tie!')
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        lossCounter += 1;
+        console.log('Rock beats scissors! Computer wins.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        winCounter += 1;
+        console.log('Scissors beats paper! You win.');
+    }
 }
 
