456c22ed970a4efccda0be258ae1b96f2fb0d784
Kylie K <kylieakrueger@gmail.com>
2022-04-13 10:33:33 -0700

    Add game function

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         console.log('It\'s a tie!');
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        lossCounter += 1;
+        winCounter -= 1;
         console.log('Paper beats rock! Computer wins.');
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         winCounter += 1;
@@ -37,18 +37,38 @@ function playRound (playerSelection, computerSelection) {
         winCounter += 1;
         console.log('Paper beats rock! You win.');
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        lossCounter += 1;
+        winCounter -= 1;
         console.log('Scissors beats paper! Computer wins.');
     }
 
     if (playerSelection === 'scissors' && computerSelection === 'scissors') {
         console.log('It\'s a tie!')
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        lossCounter += 1;
+        winCounter -= 1;
         console.log('Rock beats scissors! Computer wins.');
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         winCounter += 1;
         console.log('Scissors beats paper! You win.');
+    }
+    return winCounter;
+}
+
+function game() {
+
+    let winCount = 0;
+    for (let i = 0; i < 5; i++) {
+        let choice = prompt('Rock, paper, or scissors?');
+        let computerChoice = computerPlay();
+        winCount += playRound(choice, computerChoice);
+    }
+
+    if (winCount > 0) {
+        alert('You won the game with ' + winCount + ' round wins!');
+    } else if (winCount === 0) {
+        alert('It\'s a draw! Crazy!');
+    } else if (winCount < 0) {
+        winCount = abs(winCount);
+        alert('The computer won the game with ' + winCount + ' round wins!');
     }
 }
 
